aa62314491e597ec90d28fc003f8466b503b0474
DESKTOP-L0KFFA6\fredd <zeitzschel97@gmail.com>
2018-08-13 18:27:34 +0200

    erster button mit OnClick

--- MyApplication/app/src/main/java/flatau/krissini/de/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/flatau/krissini/de/myapplication/MainActivity.java
@@ -1,13 +1,29 @@
 package flatau.krissini.de.myapplication;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class MainActivity extends Activity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView text = findViewById(R.id.text);
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button.setText("nochmal!");
+            }
+        });
     }
 }
